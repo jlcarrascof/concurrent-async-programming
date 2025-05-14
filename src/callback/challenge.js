95a79d10a80ec54ced6882ff737fec91b2aa21f9
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-05-13 20:41:56 -0400

    Implement fetchData function with XMLHttpRequest setup

---
+++ src/callback/challenge.js
@@ -0,0 +1,7 @@
+const XMLHttpRequest = require('xmlhttprequest');
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi, callback) {
+    let xhttp = new XMLHttpRequest();
+
+}
